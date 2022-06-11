ca255a5368e1618cd99468814bcdd96df519fb3d
Jeisson Gómez <jeissongomez@hotmail.es>
2022-06-10 23:11:14 -0500

    Reset estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,9 @@
-import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  const restart = () => setCount(0);
-
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            Sumar
-          </button>
-          <p>{count}</p>
-          <button type="button" onClick={() => setCount((count) => count - 1)}>
-            Restar
-          </button>
-          <button type="button" onClick={restart}>
-            Restablecer
-          </button>
-          <button type="button" onClick={() => setCount((count) => count * 10)}>
-            X 10
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <>
+      <h2>Lista de Tareas</h2>
+    </>
   );
-}
-
-export default App;
+};
